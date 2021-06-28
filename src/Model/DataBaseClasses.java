45ca26794171250c38ed3bd86238466c8fe31867
Rehim <rehim-456@hotmail.com>
2021-06-29 00:08:17 +0400

    Create DataBaseClasses.java

---
+++ src/Model/DataBaseClasses.java
@@ -0,0 +1,17 @@
+package Model;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class DataBaseClasses {
+    public static List<Employee> Employees = new ArrayList<>();
+    public static List<InventoryEmployee> InvEmployees = new ArrayList<>();
+    public static List<Cashier> Cashiers = new ArrayList<>();
+    public static List<Manager> Managers = new ArrayList<>();
+    public static List<Customer> Customers = new ArrayList<>();
+    public static List<Owner> Owners = new ArrayList<>();
+    public static List<CashierSales> CashierSale = new ArrayList<>();
+    public static List<ShoppingCart> ShoppingCarts = new ArrayList<>();
+    public static List<Product> Products = new ArrayList<>();
+}
+
